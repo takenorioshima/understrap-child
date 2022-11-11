3b6a54b810f65eb0c29d8cfbeb0fc2b86febc4d7
Takenori Oshima <takenorioshima@gmail.com>
2022-11-11 16:51:14 +0900

    update: Remove BootStrap4 support

--- header.php
+++ header.php
@@ -9,9 +9,7 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
-$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
-$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+$navbar_type = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -29,8 +27,8 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 	<!-- ******************* The Navbar Area ******************* -->
 	<header id="wrapper-navbar">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+		<a class="visually-hidden-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
+		<?php get_template_part( 'global-templates/navbar', $navbar_type ); ?>
 
 	</header><!-- #wrapper-navbar end -->
