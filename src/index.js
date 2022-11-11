a64d13671b6659e82f15180d14fc8d3b3089dd9c
Takenori Oshima <takenorioshima@gmail.com>
2022-11-11 15:16:26 +0900

    update: Introduction of webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Import Scss.
+import "./sass/index.scss";
+
+// Import JavaScript.
+import "bootstrap";
+import "./js/main";
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: true,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: ["autoprefixer"],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+  ],
+  devtool: "source-map",
+  mode: "production",
+};
