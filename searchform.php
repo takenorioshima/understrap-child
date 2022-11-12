c50246d9608f5ebcce1989cb35047f37fdfe33f7
Takenori Oshima <takenorioshima@gmail.com>
2022-11-12 13:17:39 +0900

    delete: Drop bootstrap4 support

--- searchform.php
+++ searchform.php
@@ -7,9 +7,7 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
-$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
-$uid               = wp_unique_id( 's-' ); // The search form specific unique ID for the input.
+$uid = wp_unique_id( 's-' ); // The search form specific unique ID for the input.
 
 $aria_label = '';
 if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
@@ -21,13 +19,6 @@ if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
 	<label class="screen-reader-text" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'understrap' ); ?></label>
 	<div class="input-group">
 		<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'understrap' ); ?>">
-		<?php if ( 'bootstrap5' === $bootstrap_version ) : ?>
-			<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>">
-		<?php else : ?>
-			<span class="input-group-append">
-				<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>">
-			</span>
-		<?php endif; ?>
+		<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>">
 	</div>
 </form>
-<?php
